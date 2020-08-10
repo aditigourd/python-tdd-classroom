b43e554e984ea8b0608ddd5be543a971e17e56f4
Aditi Gour <aditi.gour@datagrokr.com>
2020-08-10 12:24:33 +0530

    implemented functions

--- src/exercises.py
+++ src/exercises.py
@@ -2,10 +2,17 @@ def reverse_list(input_list):
     """
     Reverses order of elements in list input_list.
     """
-    return None
+    newlst=input_list[::-1]
+    return newlst
 
 def count_digits(number):
     """
     Return count of digits
     """
-    return None
+    temp=number
+    count=0
+    while temp!=0:
+        count+=1
+        temp=int(temp/10)
+
+    return count
--- src/string_exercise.py
+++ src/string_exercise.py
@@ -7,25 +7,43 @@ class StringExercise:
         """
         Reverses order of characters in string input_str.
         """
-        return None
+        rev=input_str[::-1]
+        return rev
 
     def is_english_vowel(self, character):
         """
         Returns True if character is an english vowel
         and False otherwise.
         """
-        return None
+        if character=='a' or character=='e' or character=='i' or character=='o' or character=='u':
+            return True
+        if character=='A' or character=='E' or character=='I' or character=='O' or character=='U':
+            return True
+        return False
 
     def find_longest_word(self, sentence):
         """
         Returns the longest word in string sentence.
         In case there are several, return the first.
         """
-        return None
+        words=sentence.split(" ")
+        max=-1
+        ans=""
+        for word in words:
+            if(len(word)>max):
+                max=len(word)
+                ans=word
+        return ans
 
     def get_word_lengths(self, text):
         """
         Returns a list of integers representing
         the word lengths in string text.
         """
-        return None
+        lst=[]
+        words=text.split(" ")
+        for word in words:
+            lst.append(len(word))
+        return lst
+
+
